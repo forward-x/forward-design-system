381da9724983641d72efd12ce436371ead367f35
Goggags123 <wongsakorngag@gmail.com>
2022-04-26 16:53:33 +0700

    Adding plugin for building svg as react component

--- packages/icons/rollup.config.ts
+++ packages/icons/rollup.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
+import svgr from '@svgr/rollup';
+import url from '@rollup/plugin-url';
 
 export default defineConfig({
   input: './index.ts',
   output: [
-    { dir: 'dist', format: 'esm', sourcemap: true, preserveModules: true },
+    { dir: 'dist', format: 'commonjs', sourcemap: true, preserveModules: true },
   ],
   plugins: [
+    url(),
+    svgr({ icon: true }),
     typescript({
       declaration: true,
       declarationDir: 'dist',
