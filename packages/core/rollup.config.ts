53d872b285c1273f0dab1261172ef3c39c93e8ca
Goggags123 <wongsakorngag@gmail.com>
2022-04-29 11:22:21 +0700

    Fix rollup and eslint

--- packages/core/rollup.config.ts
+++ packages/core/rollup.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
+import url from '@rollup/plugin-url';
+import svgr from '@svgr/rollup';
+import { defineConfig } from 'rollup';
 import postcss from 'rollup-plugin-postcss';
 
 export default defineConfig({
   input: './index.ts',
   output: [
-    { dir: 'dist', format: 'esm', sourcemap: true, preserveModules: true },
+    {
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: '.',
+    },
   ],
   plugins: [
+    url(),
+    svgr({ icon: true }),
     typescript({
       declaration: true,
       declarationDir: 'dist',
