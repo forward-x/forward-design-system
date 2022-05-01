046b37d71df551391fd49db0ed9c20fb5db85a0a
Goggags123 <wongsakorngag@gmail.com>
2022-05-01 18:50:01 +0700

    Add Avatar

---
+++ packages/core/components/base/Avatar/index.tsx
@@ -0,0 +1,53 @@
+import React, { FC, HTMLProps } from 'react';
+
+import { UserIcon } from '@forward-ui/icons';
+import clsx from 'clsx';
+
+import { ReactComponent as ForwardMan } from '../../../assets/icons/ForwardMan.svg';
+
+import styles from './index.module.scss';
+export interface IAvatarProps {
+  variant: 'Default' | 'Name' | 'Forward';
+  size: 'XL' | 'L' | 'M' | 'S';
+  name?: string;
+  className?: string;
+}
+
+const Avatar: FC<IAvatarProps> = ({ variant, name, size, className }) => {
+  const avatarSize = {
+    XL: 'L',
+    L: 'S',
+    M: 'XS',
+    S: 'XXS',
+  } as const;
+
+  let content;
+  switch (variant) {
+    case 'Name':
+      content = name;
+      break;
+    case 'Forward':
+      content = <ForwardMan className={styles.icon} />;
+      break;
+    default:
+      content = (
+        <UserIcon className={styles.user_icon} size={avatarSize[size]} />
+      );
+      break;
+  }
+
+  return (
+    <div
+      className={clsx(styles.avatar, className, {
+        [styles.small]: size === 'S',
+        [styles.large]: size === 'L',
+        [styles.extra_large]: size === 'XL',
+        [styles.forward_man]: variant === 'Forward',
+      })}
+    >
+      {content}
+    </div>
+  );
+};
+
+export default Avatar;
