327231e5180dfa128a471039c436eb04d9b01cd5
Goggags123 <wongsakorngag@gmail.com>
2022-05-08 02:37:21 +0700

    Add Radio input

--- packages/core/src/components/base/Input/index.tsx
+++ packages/core/src/components/base/Input/index.tsx
@@ -1,8 +1,16 @@
+import Checkbox from './Checkbox';
 import Digits from './Digits';
 import Default from './Input';
 import Password from './Password';
+import Radio from './Radio';
 import Search from './Search';
 
-const Input = Object.assign(Default, { Password, Search, Digits });
+const Input = Object.assign(Default, {
+  Password,
+  Search,
+  Digits,
+  Checkbox,
+  Radio,
+});
 
 export default Input;
