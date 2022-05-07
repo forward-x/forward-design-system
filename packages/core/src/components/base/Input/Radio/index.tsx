1601051049f4c518773d47faa340169ff26603c4
Goggags123 <wongsakorngag@gmail.com>
2022-05-08 02:52:46 +0700

    Add light theme and mobile support to radio input

--- packages/core/src/components/base/Input/Radio/index.tsx
+++ packages/core/src/components/base/Input/Radio/index.tsx
@@ -7,16 +7,24 @@ import styles from './index.module.scss';
 export interface IRadioProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   text?: string;
+  color?: 'dark' | 'light';
 }
 
 const Radio: FC<IRadioProps> = ({
   text,
   id = 'default',
+  color = 'dark',
   className,
   ...props
 }) => {
   return (
-    <div className={clsx(styles.container, className)}>
+    <div
+      className={clsx(
+        styles.container,
+        { [styles.light]: color === 'light' },
+        className
+      )}
+    >
       <input type="radio" id={id} {...props} />
       <label htmlFor={id}>
         <div className={styles.radio} />
