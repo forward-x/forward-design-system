f22046557e817c11ff658af8b8c26fa707416739
Goggags123 <wongsakorngag@gmail.com>
2022-05-08 01:55:09 +0700

    Refactor Checkbox

--- packages/core/src/components/base/Input/Checkbox/index.tsx
+++ packages/core/src/components/base/Input/Checkbox/index.tsx
@@ -1,23 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, InputHTMLAttributes } from 'react';
 
+import { CheckmarkIcon } from '@forward-ui/icons';
 import clsx from 'clsx';
 
 import styles from './index.module.scss';
 
-export interface ICheckboxProps {
+export interface ICheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   text?: string;
-  id?: string;
-  value?: string;
 }
 
-const Checkbox: FC<ICheckboxProps> = ({ text, id, value, className }) => {
+const Checkbox: FC<ICheckboxProps> = ({
+  text,
+  id = 'default',
+  className,
+  ...props
+}) => {
   return (
     <div className={clsx(styles.container, className)}>
-      <input type="checkbox" id={id} value={value} />
+      <input type="checkbox" id={id} {...props} />
       <label htmlFor={id}>
         <div className={styles.checkbox}>
-          <div className={styles.hover_bg} />
+          <CheckmarkIcon size="XXS" className={styles.check} />
         </div>
         {text}
       </label>
