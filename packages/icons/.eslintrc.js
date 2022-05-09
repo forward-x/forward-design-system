1c6994d10860c439f9e203b0d3c39343219b6f08
Goggags123 <wongsakorngag@gmail.com>
2022-05-09 11:50:33 +0700

    Update eslints

---
+++ packages/icons/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: '../../.eslintrc.json',
+  root: true,
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: __dirname + '/tsconfig.json',
+      },
+    },
+  },
+};
