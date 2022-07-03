6a374454c92169b475254011e4e585486b6f98a3
Passakon Puttasuwan <passakon_p@hotmail.com>
2022-07-04 00:45:22 +0700

    chore: avatar

---
+++ packages/core/src/components/base/Avatar/Avatar.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { Avatar } from '@forward-protocol/uikit';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+// export interface IAvatarProps {
+//   /**
+//    * @default 'default'
+//    */
+//   variant?: 'default' | 'name' | 'forward';
+//   /**
+//    * XL - 64px
+//    *
+//    * L - 40px
+//    *
+//    * M - 32px
+//    *
+//    * S - 24px
+//    *
+//    * @default 'M'
+//    */
+//   size?: 'XL' | 'L' | 'M' | 'S';
+//   name?: string;
+//   className?: string;
+// }
+export default {
+  title: 'Base/Avatar',
+  component: Avatar,
+  argTypes: {
+    variant: {
+      options: ['default', 'name', 'forward'],
+      control: { type: 'radio' },
+      defaultValue: 'primary',
+    },
+    size: {
+      options: ['S', 'M', 'L', 'XL'],
+      control: { type: 'inline-radio' },
+      defaultValue: 'M',
+    },
+  },
+} as ComponentMeta<typeof Avatar>;
+
+const Template: ComponentStory<typeof Avatar> = (args) => (
+  <div style={{ width: '260px' }}>
+    <Avatar {...args} />
+  </div>
+);
+
+export const Default: ComponentStory<typeof Avatar> = Template.bind({});
+Default.args = {
+  variant: 'default',
+};
+
+export const ForwardMan: ComponentStory<typeof Avatar> = Template.bind({});
+ForwardMan.args = {
+  variant: 'forward',
+};
+
+export const Name: ComponentStory<typeof Avatar> = Template.bind({});
+Name.args = {
+  variant: 'name',
+  name: 'NP',
+};
