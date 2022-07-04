835b851ae11b9e1b149b5357e1856ef9c31dfb9b
Goggags123 <wongsakorngag@gmail.com>
2022-07-05 05:30:46 +0700

    fix: forwardRef on Checkbox, Password, and Radio

--- packages/core/src/components/base/Input/Checkbox/index.tsx
+++ packages/core/src/components/base/Input/Checkbox/index.tsx
@@ -1,4 +1,9 @@
-import React, { FC, InputHTMLAttributes, ReactNode, useId } from 'react';
+import React, {
+  forwardRef,
+  InputHTMLAttributes,
+  ReactNode,
+  useId,
+} from 'react';
 
 import { CheckmarkIcon } from '@forward-protocol/ui-icons';
 import clsx from 'clsx';
@@ -6,7 +11,7 @@ import clsx from 'clsx';
 import styles from './index.module.scss';
 
 export interface ICheckboxProps
-  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size' | 'type'> {
   className?: string;
   text?: ReactNode;
   /**
@@ -19,32 +24,30 @@ export interface ICheckboxProps
   size?: 'M' | 'S';
 }
 
-const Checkbox: FC<ICheckboxProps> = ({
-  size = 'M',
-  text,
-  id,
-  className,
-  ...props
-}) => {
-  const randomId = useId();
+const Checkbox = forwardRef<HTMLInputElement | null, ICheckboxProps>(
+  ({ size = 'M', text, id, className, ...props }, ref) => {
+    const randomId = useId();
 
-  return (
-    <div
-      className={clsx(
-        styles.container,
-        { [styles.small]: size === 'S' },
-        className
-      )}
-    >
-      <input type="checkbox" id={id ?? randomId} {...props} />
-      <label htmlFor={id ?? randomId}>
-        <div className={styles.checkbox}>
-          <CheckmarkIcon className={styles.check} />
-        </div>
-        {text}
-      </label>
-    </div>
-  );
-};
+    return (
+      <div
+        className={clsx(
+          styles.container,
+          { [styles.small]: size === 'S' },
+          className
+        )}
+      >
+        <input type="checkbox" id={id ?? randomId} {...props} ref={ref} />
+        <label htmlFor={id ?? randomId}>
+          <div className={styles.checkbox}>
+            <CheckmarkIcon className={styles.check} />
+          </div>
+          {text}
+        </label>
+      </div>
+    );
+  }
+);
+
+Checkbox.displayName = 'Checkbox';
 
 export default Checkbox;
--- packages/core/src/components/base/Input/Radio/index.tsx
+++ packages/core/src/components/base/Input/Radio/index.tsx
@@ -1,26 +1,36 @@
-import React, { FC, InputHTMLAttributes, ReactNode, useId } from 'react';
+import React, {
+  forwardRef,
+  InputHTMLAttributes,
+  ReactNode,
+  useId,
+} from 'react';
 
 import clsx from 'clsx';
 
 import styles from './index.module.scss';
 
-export interface IRadioProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface IRadioProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {
   className?: string;
   text?: ReactNode;
 }
 
-const Radio: FC<IRadioProps> = ({ text, id, className, ...props }) => {
-  const randomId = useId();
+const Radio = forwardRef<HTMLInputElement | null, IRadioProps>(
+  ({ text, id, className, ...props }, ref) => {
+    const randomId = useId();
 
-  return (
-    <div className={clsx(styles.container, className)}>
-      <input type="radio" id={id ?? randomId} {...props} />
-      <label htmlFor={id ?? randomId}>
-        <div className={styles.radio} />
-        {text}
-      </label>
-    </div>
-  );
-};
+    return (
+      <div className={clsx(styles.container, className)}>
+        <input type="radio" id={id ?? randomId} {...props} ref={ref} />
+        <label htmlFor={id ?? randomId}>
+          <div className={styles.radio} />
+          {text}
+        </label>
+      </div>
+    );
+  }
+);
+
+Radio.displayName = 'Radio';
 
 export default Radio;
